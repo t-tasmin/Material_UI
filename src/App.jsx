fe10870dcfb9453f0afcb15a83f067f890751576
Tania Tasmin <taniat.kaau@email.com>
2022-02-04 16:54:05 -0500

    Create App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,18 @@
 import React from 'react';
-import { Button } from '@material-ui/core';
-import { Person, Delete, Send} from '@material-ui/icons';
 import { makeStyles } from '@material-ui/styles';
+import Navbar from './components/Navbar';
 
 const useStyles =makeStyles((theme)=>({
-  button: {
-    color:"yellow",
-    backgroundColor: "purple",
-  }
+  
 }));
 
 export default function App () {
   
   const classes = useStyles();
+
   return (
     <div>
-       <Button variant="contained" color="primary" size="large"  
-         endIcon={<Send/>}  className={classes.button}>
-         Material UI
-       </Button>
+       <Navbar/>
     </div>
   );
 
